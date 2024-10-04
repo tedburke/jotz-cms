6d5c976094c29a7faec69f319535c70dc8279e4b
tedburke <teddy.burke@gmail.com>
2024-10-04 12:07:04 +0200

    Added code to index.php to implement separator horizontal rule in list of posts when a dummy article with the title '#SEPARATOR' appears.

--- jotz/index.php
+++ jotz/index.php
@@ -6,22 +6,30 @@ $arrFiles = scandir("markdown", SCANDIR_SORT_DESCENDING);
 foreach ($arrFiles as $filename)
 {
   if (substr($filename, -3) != '.md') continue;
-  $title = "";
+  $article_title = "";
   $lines = file("markdown/" . $filename);
   foreach ($lines as $line)
   {
     $line = trim($line);
     if (substr($line, 0, 6) == "title:")
     {
-      $title = trim(substr($line, 6));
+      $article_title = trim(substr($line, 6));
       break;
     }
   }
+  $article_date = date("Y-m-d", filemtime("markdown/" . $filename)); 
 
-  echo "<div style=\"clear:both;\">" . PHP_EOL;
-  echo "<div style=\"width:8em;text-align:center;float:left;\">" . date("Y-m-d", filemtime("markdown/" . $filename)) . "</div>" . PHP_EOL;
-  echo "<div style=\"width:calc(100% - 9em);float:left;\"><a href=\"view.php?mdfile=$filename\">$title</a></div>" . PHP_EOL;
-  echo "</div>" . PHP_EOL;
+  if ($article_title == "#SEPARATOR")
+  {
+    echo "<div style=\"clear:both;padding:0.75em 15% 0.5em 15%;\"><hr></div>" . PHP_EOL;
+  }
+  else
+  {
+    echo "<div style=\"clear:both;padding:0.5em 0 0.5em 0;\">" . PHP_EOL;
+    echo "<div style=\"width:8em;text-align:center;float:left;\">" . $article_date . "</div>" . PHP_EOL;
+    echo "<div style=\"width:calc(100% - 9em);float:left;\"><a href=\"view.php?mdfile=$filename\">$article_title</a></div>" . PHP_EOL;
+    echo "</div>" . PHP_EOL;
+  }
 }
 
 include("footer.php");
