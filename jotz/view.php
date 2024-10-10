fa9a31cdf2444dd16e399b1ba8e5cc113ab7737b
tedburke <teddy.burke@gmail.com>
2024-10-10 19:02:26 +0200

    Re-indented view.php to fix inconsistent mixture of tabs and spaces.

--- jotz/view.php
+++ jotz/view.php
@@ -8,59 +8,59 @@ include("header.php");
 $arrFiles = scandir("markdown", SCANDIR_SORT_DESCENDING);
 foreach ($arrFiles as $filename)
 {
-	$article_id = "no_id";
-	$article_title = "untitled";
-	$article_markdown = "";
- 
+    $article_id = "no_id";
+    $article_title = "untitled";
+    $article_markdown = "";
+
     // If a specific markdown file was specified, skip other files
     if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['mdfile']))
     {
-      if (basename($filename) != $_GET['mdfile']) continue;
+        if (basename($filename) != $_GET['mdfile']) continue;
     }
-	
-	// Skip non-markdown files
-	if (substr($filename, -3) != '.md') continue;
-	
-	$article_id = substr($filename, 0, -3);
 
-	// Parse markdown file using state machine.
-	$lines = file("markdown/" . $filename);
-	$parse_state = 0;
-	foreach ($lines as $line)
-	{
-		if ($parse_state == 0) // State 0 until start of metadata
-		{
-			if (trim($line) == "---") $parse_state = 1;
-		}
-		else if ($parse_state == 1) // State 1 until title is found
-		{
-			$line = trim($line);
-			if (substr($line, 0, 6) == 'title:')
-			{
+    // Skip non-markdown files
+    if (substr($filename, -3) != '.md') continue;
+
+    $article_id = substr($filename, 0, -3);
+
+    // Parse markdown file using state machine.
+    $lines = file("markdown/" . $filename);
+    $parse_state = 0;
+    foreach ($lines as $line)
+    {
+        if ($parse_state == 0) // State 0 until start of metadata
+        {
+            if (trim($line) == "---") $parse_state = 1;
+        }
+        else if ($parse_state == 1) // State 1 until title is found
+        {
+            $line = trim($line);
+            if (substr($line, 0, 6) == 'title:')
+            {
                 $article_title = trim(substr($line, 6));
-				$parse_state = 2;
-			}
-		}
-		else if ($parse_state == 2) // State 2 until end of metadata
-		{
-			if (trim($line) == "..." or trim($line) == "---") $parse_state = 3;			
-		}
-		else if ($parse_state == 3) // State 3 until first non-blank line of markdown
-		{
-			if (trim($line) != "")
-			{
-				$parse_state = 4;
-				$article_markdown .= $line;
-			}
-		}
-		else if ($parse_state == 4) // State 4 once markdown has started
-		{
-			$article_markdown .= $line;
-		}
-	}
-	
-	if ($parse_state < 3) echo ("<p>ERROR: Incomplete metadata in $filename</p>");
-  
+                $parse_state = 2;
+            }
+        }
+        else if ($parse_state == 2) // State 2 until end of metadata
+        {
+            if (trim($line) == "..." or trim($line) == "---") $parse_state = 3;			
+        }
+        else if ($parse_state == 3) // State 3 until first non-blank line of markdown
+        {
+            if (trim($line) != "")
+            {
+                $parse_state = 4;
+                $article_markdown .= $line;
+            }
+        }
+        else if ($parse_state == 4) // State 4 once markdown has started
+        {
+            $article_markdown .= $line;
+        }
+    }
+
+    if ($parse_state < 3) echo ("<p>ERROR: Incomplete metadata in $filename</p>");
+
     if ($article_title == "#SEPARATOR")
     {
         // This is a dummy article included as a separator, so don't
